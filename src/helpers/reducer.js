c7ed83b80c6a22a9d73042d68f4cf013471a1fed
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-12 23:35:44 -0400

    Added remove individual item functionality

--- src/helpers/reducer.js
+++ src/helpers/reducer.js
@@ -2,6 +2,13 @@ const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return { ...state, cart: [] };
   }
+  if (action.type === 'REMOVE') {
+    let newCartItems = state.cart.filter(item => item.id !== action.payload);
+    return {
+      ...state,
+      cart: newCartItems,
+    };
+  }
   throw new Error('no matching action type');
 };
 
