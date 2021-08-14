f7ce47faf416f2788abdef084215b67a335d5d13
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-13 20:30:14 -0400

    Added increase item functionality

--- src/helpers/reducer.js
+++ src/helpers/reducer.js
@@ -4,10 +4,16 @@ const reducer = (state, action) => {
   }
   if (action.type === 'REMOVE') {
     let newCartItems = state.cart.filter(item => item.id !== action.payload);
-    return {
-      ...state,
-      cart: newCartItems,
-    };
+    return { ...state, cart: newCartItems };
+  }
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map(item => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
   }
   throw new Error('no matching action type');
 };
